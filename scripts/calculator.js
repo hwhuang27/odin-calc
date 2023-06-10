96d30dbb8343afcf0ad92fc43c45365d3b9aae75
DavidHuang <david.huang3214@gmail.com>
2023-06-10 16:45:28 -0700

    Create calculator UI

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -17,22 +17,41 @@ function operate(operator, a, b){
 }
 
 let buttonText = [
-    'C', '%', '←', '÷',
-    '7', '8', '9', 'x',
-    '4', '5', '6', '-',
-    '1', '2', '3', '+',
-    '+/-', '0', '.', '='
+    'C', '%', '←', '7',
+    '8', '9', '÷', '4',
+    '5', '6', 'x', '1',
+    '2', '3', '-', '+/-',
+    '0', '.', '+', '='
 ]
 
 let buttons = document.querySelector('.container');
+let display = document.querySelector('.display');
 
 buttonText.forEach(symbol => {
     const button = document.createElement('button');
     button.textContent = symbol;
     button.setAttribute('type', 'button');
     button.classList.add('calc-button');
-
-    buttons.appendChild(button);    
+    switch(symbol){
+        case '%':
+        case '←':
+        case '÷': 
+        case 'x': 
+        case '-':
+        case '+':
+            button.classList.add('calc-symbol');
+            break;
+        case 'C':
+            button.classList.add('calc-clear');
+            break;
+        case '=':
+            button.classList.add('calc-equals');
+            break;
+    }
+    button.addEventListener('click', () => display.textContent += symbol);
+    buttons.appendChild(button);
 });
 
-
+function setDisplay(symbol){
+    
+}
